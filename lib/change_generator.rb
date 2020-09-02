b6263c26bed36ed642a9edab065a104017825e48
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-09-02 13:01:03 +0100

    Refactor.

--- lib/change_generator.rb
+++ lib/change_generator.rb
@@ -1,6 +1,8 @@
 
 class ChangeGenerator
   def convert(number)
-    number == 1 ? ["£1"] : number == 2 ? ["£2"] : ["50p"]
+    change = []
+    number == 1 ? change << "£#{1}" : number == 2 ? change << "£#{2}" : change << "50p"
+
   end
 end
