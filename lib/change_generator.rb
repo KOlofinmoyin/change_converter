9127ee01ff80c3e315b55b45ac662ea9db5c0210
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-09-04 11:38:02 +0100

    takes '40' and returns ['£20', '£20']
    Co-authored-by: Emily Wright <emilywright212@gmail.com>

--- lib/change_generator.rb
+++ lib/change_generator.rb
@@ -2,6 +2,9 @@ class ChangeGenerator
   def convert(number)
     money = []
     pence = (number * 100);
+    calculate_twenty_pounds = pence.to_i / 2000;
+    calculate_twenty_pounds.times { money << "£20" }
+    calculate_twenty_pounds.times { pence -= 2000 }
     calculate_five_pounds = pence.to_i / 500;
     calculate_five_pounds.times { money << "£5" }
     calculate_five_pounds.times { pence -= 500 }
@@ -22,46 +25,6 @@ class ChangeGenerator
     calculate_two_pence.times { pence -= 2 }
 
     p money
-
-    # if number.is_a? Float
-    #   # number < 1 ? money << "#{pence}p" : money << "£#{pounds}" && money << "#{pence}p"
-    #
-    #   calculate_five_pounds.times { money << "£5" }
-    #   p money
-    #   p "£"
-    #   p pounds
-    #   p pence
-    #   calculate_one_pounds.times { money << "£1" }
-    #   p money
-    #   p "£"
-    #   p pounds
-    #   p pence
-    #   calculate_fifty_pence.times { money << "50p" }
-    #   p money
-    #   p "£"
-    #   p pounds
-    #   p pence
-    #   calculate_twenty_pence.times { money << "20p" }
-    #   calculate_twenty_pence.times {pence -= 20}
-    #   p money
-    #   p "£"
-    #   p pounds
-    #   p pence
-    #   calculate_ten_pence.times { money << "10p" }
-    #   p money
-    #   p "£"
-    #   p pounds
-    #   p pence
-    #   calculate_two_pence.times { money << "2p" }
-    #   p money
-    #   p "£"
-    #   p pounds
-    #   p pence
-    # else
-    #   # number == 1 ? money << "£#{number}" : number == 2 ? money << "£#{number}" : money << "50p"
-    #   calculate_five_pounds.times { money << "£5" } && calculate_one_pounds.times { money << "£1" }
-    #   p money
-    # end
 
   end
 end
--- spec/change_generator_spec.rb
+++ spec/change_generator_spec.rb
@@ -14,13 +14,17 @@ describe ChangeGenerator do
   it "takes '0.50' and returns ['50p']" do
     expect(change_generator.convert(0.50)).to eq (["50p"])
   end
-  
+
   it "takes '1.50' and returns ['£1', '50p']" do
     expect(change_generator.convert(1.50)).to eq (["£1", "50p"])
   end
 
   it "takes '7.42' and returns ['£5', '£1', '£1', '20p', '20p', '2p']" do
     expect(change_generator.convert(7.42)).to eq (["£5", "£1", "£1", "20p", "20p", "2p"])
+  end
+
+  it "takes '40' and returns ['£20', '£20']" do
+    expect(change_generator.convert(40)).to eq (["£20", "£20"])
   end
 
 end
